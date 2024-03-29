067be786352f8d8f579ff34f6d2e896f57b776d0
Cindy Nilsson <dante.a.nilsson@gmail.com>
2024-03-30 00:13:41 +0100

    Adding tests

--- parser/src/lexer.rs
+++ parser/src/lexer.rs
@@ -6,21 +6,21 @@ pub enum Token {
     Ident(String),
     NumberLiteral(f64),
     CommandPrefix,
-    ExpressionBegin, // {
-    ExpressionEnd,   // }
-    BracketBegin,    // [
-    BracketEnd,      // ]
-    ParenthesisBegin,
-    ParenthesisEnd,
-    Negative,
-    Apostrofy,
-    Underscore,
-    Caret,
-    Mul,
-    Add,
-    Div,
-    VerticalPipe,
-    EOF,
+    ExpressionBegin,  // {
+    ExpressionEnd,    // }
+    BracketBegin,     // [
+    BracketEnd,       // ]
+    ParenthesisBegin, // (
+    ParenthesisEnd,   // )
+    Negative,         // -
+    Apostrofy,        // '
+    Underscore,       // _
+    Caret,            // ^
+    Mul,              // *
+    Add,              // +
+    Div,              // \
+    VerticalPipe,     // ｜ ik its tipping
+    EOF,              // \EOF
 }
 
 impl Token {
@@ -73,6 +73,8 @@ impl Lexer {
                 '*' => Token::Mul,
                 '+' => Token::Add,
                 '/' => Token::Div,
+                '(' => Token::ParenthesisBegin,
+                ')' => Token::ParenthesisEnd,
                 ' ' => {
                     if !temp_number.is_empty() {
                         let num = Token::NumberLiteral(
@@ -119,6 +121,19 @@ impl Lexer {
 
             self.send_or_crach(t).await;
         }
+        if !temp_number.is_empty() {
+            let num = Token::NumberLiteral(
+                temp_number
+                    .parse()
+                    .expect("THIS NEEDS FIXING IT FAILED TO PARSE NUMBER"),
+            );
+            temp_number = String::new();
+            self.send_or_crach(num).await;
+        }
+        if !temp_ident.is_empty() {
+            self.send_or_crach(Token::Ident(take(&mut temp_ident)))
+                .await;
+        }
         self.send_or_crach(Token::EOF).await;
     }
 }
@@ -148,7 +163,7 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn test1() {
+    async fn test_simple_sqrt() {
         assert_eq!(
             tokenize("\\sqrt{1+2x}").await,
             vec![
@@ -164,33 +179,108 @@ mod tests {
         );
     }
     #[tokio::test]
-    async fn test1_fail() {
+    async fn test_all_simple_operations() {
         assert_ne!(
-            tokenize("\\sqrt{1+2x}").await,
+            tokenize("-+*/").await,
             vec![
-                Token::Ident("sqrt".to_string()),
-                Token::ExpressionBegin,
-                Token::NumberLiteral(1.0),
+                Token::Negative,
                 Token::Add,
-                Token::NumberLiteral(2.0),
-                Token::Ident("x".to_string()),
-                Token::ExpressionEnd,
+                Token::Mul,
+                Token::Div
             ]
         );
     }
 
     #[tokio::test]
-    async fn benchsqrt() {
+    async fn test_single_character_tokens() {
         assert_eq!(
-            tokenize("\\sqrt{1+2x}").await,
+            tokenize("()[]{}^'|").await,
             vec![
+                Token::ParenthesisBegin,
+                Token::ParenthesisEnd,
+                Token::BracketBegin,
+                Token::BracketEnd,
+                Token::ExpressionBegin,
+                Token::ExpressionEnd,
+                Token::Caret,
+                Token::Apostrofy,
+                Token::VerticalPipe,
+            ]
+        );
+    }
+    #[tokio::test]
+    async fn test_number_literals() {
+        assert_eq!(
+            tokenize("3.14 42").await,
+            vec![Token::NumberLiteral(3.14), Token::NumberLiteral(42.0),]
+        );
+    }
+    #[tokio::test]
+    async fn test_identifiers_and_commands() {
+        assert_eq!(
+            tokenize("\\pi R").await,
+            vec![
+                Token::CommandPrefix,
+                Token::Ident("pi".to_string()),
+                Token::Ident("R".to_string()),
+            ]
+        );
+        #[tokio::test]
+        async fn test_complex_expressions() {
+            assert_eq!(
+                tokenize("{3.14*R^2}").await,
+                vec![
+                    Token::ExpressionBegin,
+                    Token::NumberLiteral(3.14),
+                    Token::Mul,
+                    Token::Ident("R".to_string()),
+                    Token::Caret,
+                    Token::NumberLiteral(2.0),
+                    Token::ExpressionEnd,
+                ]
+            );
+        }
+    }
+    #[tokio::test]
+    async fn test_number_followed_by_identifier() {
+        assert_eq!(
+            tokenize("42x + 3.14y").await,
+            vec![
+                Token::NumberLiteral(42.0),
+                Token::Ident("x".to_string()),
+                Token::Add,
+                Token::NumberLiteral(3.14),
+                Token::Ident("y".to_string()),
+            ]
+        );
+    }
+    #[tokio::test]
+    async fn test_number_followed_by_command() {
+        assert_eq!(
+            tokenize("3.14\\piR").await,
+            vec![
+                Token::NumberLiteral(3.14),
+                Token::CommandPrefix,
+                Token::Ident("piR".to_string()),
+            ]
+        );
+    }
+    #[tokio::test]
+    async fn test_mixed_number_and_text_sequences() {
+        assert_eq!(
+            tokenize("2a + 4b - 5\\sqrt{c}").await,
+            vec![
+                Token::NumberLiteral(2.0),
+                Token::Ident("a".to_string()),
+                Token::Add,
+                Token::NumberLiteral(4.0),
+                Token::Ident("b".to_string()),
+                Token::Negative,
+                Token::NumberLiteral(5.0),
                 Token::CommandPrefix,
                 Token::Ident("sqrt".to_string()),
                 Token::ExpressionBegin,
-                Token::NumberLiteral(1.0),
-                Token::Add,
-                Token::NumberLiteral(2.0),
-                Token::Ident("x".to_string()),
+                Token::Ident("c".to_string()),
                 Token::ExpressionEnd,
             ]
         );
