2d129a61d16e7562dac671622e0958395a2d788d
Alvinn8 <42838560+Alvinn8@users.noreply.github.com>
2024-03-29 17:37:57 +0100

    Failed attempt at adding tests

--- parser/src/lexer.rs
+++ parser/src/lexer.rs
@@ -1,5 +1,7 @@
 use std::mem::take;
 use tokio::sync::mpsc::Sender;
+
+#[derive(PartialEq, Eq)]
 pub enum Token {
     Ident(String),
     CommandPrefix,
@@ -16,11 +18,11 @@ pub enum Token {
     Div,
     VerticalPipe,
 }
-struct Tokenizer {
+struct Lexer {
     chanel: Sender<Token>,
 }
 
-impl Tokenizer {
+impl Lexer {
     async fn tokenize(&self, s: &str) {
         let mut temp = String::new();
         for c in s.chars() {
@@ -54,4 +56,38 @@ fn token(c: char, temp: &mut String) -> Option<Token> {
             return None;
         }
     })
+}
+
+#[cfg(test)]
+mod tests {
+    use tokio::sync::mpsc::{self, Receiver, Sender};
+
+    use crate::lexer::{token, Lexer};
+
+    use super::Token;
+
+    async fn tokenize(text: &str) -> Vec<Token> {
+        let (tx, rx): (Sender<Token>, Receiver<Token>) = mpsc::channel(32); // idk what that 32 means tbh
+        let lexer = Lexer { chanel: tx };
+
+        lexer.tokenize(text).await;
+
+        todo!("Tokenize and give me the tokens in a Vec so I can compare them easialy pls ty");
+    }
+
+    #[test]
+    async fn test1() {
+        assert_eq!(
+            tokenize("\\sqrt{1+2x}").await,
+            vec![
+                Token::CommandPrefix,
+                Token::Ident("sqrt".to_string()),
+                Token::ExpressionBegin,
+                Token::Ident("1".to_string()),
+                Token::Add,
+                Token::Ident("2x".to_string()),
+                Token::ExpressionEnd,
+            ]
+        );
+    }
 }
