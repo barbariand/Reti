41ebb38aa9b190a332933483552150cad728e88b
Alvinn8 <42838560+Alvinn8@users.noreply.github.com>
2024-03-30 19:32:45 +0100

    Restructure AST

--- parser/src/ast.rs
+++ parser/src/ast.rs
@@ -1,8 +1,53 @@
-use self::mathexpr::MathExpr;
+use crate::token::Token;
 
 pub mod mathexpr;
 
-pub enum AST {
-    MathExpr(MathExpr),
-    Ignore(String), // other things than math expressions?
+#[derive(PartialEq, Debug)]
+pub struct AST {
+    root_expr: MathExpr,
+}
+
+#[derive(PartialEq, Debug)]
+pub enum MathExpr {
+    Term(Term),
+    Add(Box<MathExpr>, Term),
+    Subtract(Box<MathExpr>, Term),
+}
+
+#[derive(PartialEq, Debug)]
+pub enum Term {
+    Factor(Factor),
+    Multiply(Box<Term>, Factor),
+    Divide(Box<Term>, Factor),
+}
+
+#[derive(PartialEq, Debug)]
+pub enum Factor {
+    Constant(f64),
+    Expression(Box<MathExpr>),
+    Variable(MathIdentifier),
+    FunctionCall(FunctionCall),
+    Exponent {
+        base: Box<Factor>,
+        exponent: Box<MathExpr>,
+    },
+    Root {
+        degree: Option<Box<MathExpr>>,
+        radicand: Box<MathExpr>,
+    },
+    Abs(Box<MathExpr>),
+}
+
+/// A mathematical identifier, for example variable or function names.
+///
+/// Examples of valid math identifiers: "x", "x_1", "F_g", "\overline{v}".
+#[derive(PartialEq, Debug)]
+pub struct MathIdentifier {
+    tokens: Vec<Token>,
+}
+
+#[derive(PartialEq, Debug)]
+pub struct FunctionCall {
+    pub function_name: MathIdentifier,
+    pub arguments: Vec<MathExpr>,
 }
