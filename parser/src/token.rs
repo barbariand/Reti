f6a436f356b5c4c11d4704d4ec9d4f81e8c4474f
Cindy Nilsson <cindynilsson@duck.com>
2024-03-31 15:37:27 +0200

    Implement From<&str> for NumberLiteral

--- parser/src/token.rs
+++ parser/src/token.rs
@@ -19,7 +19,7 @@ pub enum Token {
     Underscore,        // _
     Caret,             // ^
     VerticalPipe,      // | and |
-    EndOfContent,               // A special token that represents the end of content.
+    EndOfContent,      // A special token that represents the end of content.
 }
 
 impl Token {
@@ -35,54 +35,74 @@ impl Token {
             _ => None,
         }
     }
-
+    pub fn is_eof(&self) -> bool {
+        self == Token::EndOfContent
+    }
     /// Determine if the token marks the end of an expression.
     pub fn is_end(&self) -> bool {
-        matches!(self, Token::RightCurlyBracket | Token::RightBracket | Token::RightParenthesis | Token::EndOfContent)
+        matches!(
+            self,
+            Token::RightCurlyBracket
+                | Token::RightBracket
+                | Token::RightParenthesis
+                | Token::EndOfContent
+        )
     }
 }
-impl PartialEq<Token> for &Token{
+impl PartialEq<Token> for &Token {
     fn eq(&self, other: &Token) -> bool {
-        **self==*other
+        **self == *other
     }
 }
-impl PartialEq<&Token> for Token{
+impl PartialEq<&Token> for Token {
     fn eq(&self, other: &&Token) -> bool {
-        *self==**other
+        *self == **other
     }
 }
-#[derive(Debug,Clone)]
-pub struct NumberLiteral{
-    raw:String,
-    pub parsed:f64,
+#[derive(Debug, Clone)]
+pub struct NumberLiteral {
+    raw: String,
+    pub parsed: f64,
 }
-impl PartialEq for NumberLiteral{
+impl PartialEq for NumberLiteral {
     fn eq(&self, other: &Self) -> bool {
         self.raw == other.raw
     }
 }
-impl Eq for NumberLiteral{
+impl Eq for NumberLiteral {}
 
-}
-
-impl FromStr for NumberLiteral{
-    type Err=ParseFloatError;
+impl FromStr for NumberLiteral {
+    type Err = ParseFloatError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let i:f64=s.parse()?;
-        Ok(Self{
-            raw:s.to_owned(),
+        let i: f64 = s.parse()?;
+        Ok(Self {
+            raw: s.to_owned(),
             parsed: i,
         })
-
     }
 }
-impl From<String> for NumberLiteral{
+impl From<&str> for NumberLiteral {
+    fn from(value: &str) -> Self {
+        Self {
+            parsed: value
+                .parse()
+                .expect("THIS NEEDS FIXING IT FAILED TO PARSE NUMBER"),
+            raw: value.to_owned(),
+        }
+    }
+}
+impl From<String> for NumberLiteral {
     fn from(value: String) -> Self {
-        Self {parsed: value.parse().expect("THIS NEEDS FIXING IT FAILED TO PARSE NUMBER"),raw:value }
+        Self {
+            parsed: value
+                .parse()
+                .expect("THIS NEEDS FIXING IT FAILED TO PARSE NUMBER"),
+            raw: value,
+        }
     }
 }
-impl From<usize> for NumberLiteral{
+impl From<usize> for NumberLiteral {
     fn from(value: usize) -> Self {
         value.to_string().into()
     }
