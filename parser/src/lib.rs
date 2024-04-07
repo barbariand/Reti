6f4eaeddbabae0e048648ce6b4ec6b2a382602c6
Cindy Nilsson <cindynilsson@duck.com>
2024-04-07 22:56:59 +0200

    moving functions and adding rustyline

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,10 +1,72 @@
 #![allow(dead_code)]
 
+use std::fmt::Display;
+
+use ast::Ast;
+use context::MathContext;
+
+use lexer::Lexer;
+use normalizer::Normalizer;
+use parser::{ParseError, Parser};
+use token::Token;
+use tokio::{
+    sync::mpsc::{self, Receiver, Sender},
+    task::JoinError,
+};
+
 pub mod ast;
-pub mod context;
-pub mod evaluator;
-pub mod lexer;
-pub mod normalizer;
-pub mod parser;
+mod context;
+mod evaluator;
+mod lexer;
+mod normalizer;
+mod parser;
 pub mod token;
-pub mod token_reader;
+mod token_reader;
+
+pub async fn parse(text: &str) -> Result<Ast, Error> {
+    let (lexer_in, lexer_out): (Sender<Token>, Receiver<Token>) = mpsc::channel(32);
+    let (normalizer_in, normalizer_out): (Sender<Token>, Receiver<Token>) = mpsc::channel(32);
+
+    let context = MathContext::new();
+    let lexer = Lexer::new(lexer_in);
+    let mut normalizer = Normalizer::new(lexer_out, normalizer_in);
+    let mut parser = Parser::new(normalizer_out, context);
+    let cloned_text = text.to_owned();
+    let lexer_handle = tokio::spawn(async move { lexer.tokenize(&cloned_text).await });
+    let normalizer_handle = tokio::spawn(async move { normalizer.normalize().await });
+    let parser_handle = tokio::spawn(async move { parser.parse().await });
+    match lexer_handle.await {
+        Err(e) => {
+            normalizer_handle.abort();
+            parser_handle.abort();
+            Err(Error::Lexer(e))
+        }
+        Ok(()) => match normalizer_handle.await {
+            Err(e) => {
+                parser_handle.abort();
+                Err(Error::Normalizer(e))
+            }
+            Ok(()) => match parser_handle.await {
+                Err(e) => Err(Error::Parser(e)),
+                Ok(ast) => ast.map_err(Error::ParseError),
+            },
+        },
+    }
+}
+#[derive(Debug)]
+pub enum Error {
+    Lexer(JoinError),
+    Normalizer(JoinError),
+    Parser(JoinError),
+    ParseError(ParseError),
+}
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::Lexer(e) => write!(f, "lexer thread failed beacuse: {}", e),
+            Error::Normalizer(e) => write!(f, "normalizer thread failed beacuse: {}", e),
+            Error::Parser(e) => write!(f, "parser thread failed beacuse: {}", e),
+            Error::ParseError(e) => write!(f, "Failed to parse string beacuse: {}", e),
+        }
+    }
+}
--- parser/src/main.rs
+++ parser/src/main.rs
@@ -1,11 +1,8 @@
 use clap::{command, Parser as ClapParser};
-use parser::{
-    context::MathContext, lexer::Lexer, normalizer::Normalizer, parser::Parser, token::Token,
-};
-use tokio::{
-    join,
-    sync::mpsc::{self, Receiver, Sender},
-};
+use colored::Colorize;
+
+use rustyline::{error::ReadlineError, DefaultEditor};
+
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 #[tokio::main]
@@ -29,73 +26,62 @@ struct Prompt {
     #[arg(default_value_t=LevelFilter::WARN)]
     tracing_level: LevelFilter,
 }
-
+use parser::parse;
 impl Prompt {
     async fn start(&mut self) {
-        println!("Welcome to the Reti prompt.");
-        println!("Type 1+1 and press Enter to get started.");
+        let mut rl = DefaultEditor::new().expect("Could not make terminal");
+        if rl.load_history("~/history.txt").is_err() {
+            println!("No previous history.");
+        }
+        println!("{}", "Welcome to the Reti prompt.".green());
+        println!("{}", "Type 1+1 and press Enter to get started.".green());
         println!();
 
-        let stdin = std::io::stdin();
-        let mut buf = String::new();
         loop {
-            buf.clear();
-            stdin
-                .read_line(&mut buf)
-                .expect("Failed to read from stdin.");
+            let readline = rl.readline(&format!("{}", ">> ".white()));
+            match readline {
+                Ok(line) => {
+                    rl.add_history_entry(line.as_str())
+                        .expect("could not add history");
 
-            let trimmed = buf.trim();
-            if trimmed.len() == 0 {
-                println!();
-                continue;
-            }
+                    let trimmed = line.trim();
+                    if trimmed.is_empty() {
+                        println!();
+                        continue;
+                    }
 
-            let lowercase = trimmed.to_ascii_lowercase();
-            if lowercase == "ast" {
-                self.ast_mode = !self.ast_mode;
-                match self.ast_mode {
-                    true => println!("Enabled AST mode."),
-                    false => println!("Disabled AST mode."),
+                    let lowercase = trimmed.to_ascii_lowercase();
+                    if lowercase == "ast" {
+                        self.ast_mode = !self.ast_mode;
+                        match self.ast_mode {
+                            true => println!("{}", "Enabled AST mode.".green()),
+                            false => println!("{}", "Disabled AST mode.".green()),
+                        }
+                        continue;
+                    }
+                    match parse(&line).await {
+                        Ok(v) => {
+                            if self.ast_mode {
+                                println!("{:#?}", v); //TODO fix some display for the tree
+                            };
+                            println!("> {}", v.eval())
+                        }
+                        Err(e) => println!("Got an error during parsing: {}", e.to_string().red()),
+                    };
                 }
-                continue;
+                Err(ReadlineError::Interrupted) => {
+                    println!("CTRL-C");
+                    break;
+                }
+                Err(ReadlineError::Eof) => {
+                    println!("CTRL-D");
+                    break;
+                }
+                Err(err) => {
+                    println!("Error: {:?}", err);
+                    break;
+                }
             }
-
-            self.run(trimmed).await;
         }
-    }
-
-    async fn run(&self, text: &str) {
-        let (lexer_in, lexer_out): (Sender<Token>, Receiver<Token>) = mpsc::channel(32);
-        let (normalizer_in, normalizer_out): (Sender<Token>, Receiver<Token>) = mpsc::channel(32);
-
-        let context = MathContext::new();
-        let lexer = Lexer::new(lexer_in);
-        let mut normalizer = Normalizer::new(lexer_out, normalizer_in);
-        let mut parser = Parser::new(normalizer_out, context);
-
-        let future1 = lexer.tokenize(text);
-        let future2 = normalizer.normalize();
-        let future3 = parser.parse();
-
-        let (_, _, ast) = join!(future1, future2, future3);
-        let ast = match ast {
-            Ok(ast) => ast,
-            Err(err) => {
-                println!("Failed to parse:");
-                println!("{:?}", err); // TODO impl Display
-                println!();
-                return;
-            }
-        };
-
-        if self.ast_mode {
-            println!("{:#?}", ast);
-        }
-
-        let result = ast.eval();
-
-        println!("> {}", result);
-
-        println!();
     }
 }
