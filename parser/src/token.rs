9539a369900f0f29adb83e276204e22bd222f86d
Cindy Nilsson <dante.a.nilsson@gmail.com>
2024-05-09 22:22:21 +0200

    Token Documentation

--- parser/src/token.rs
+++ parser/src/token.rs
@@ -1,31 +1,109 @@
+//!All valid token types in the TokenStream
 use std::{fmt::Display, hash::Hash, num::ParseFloatError, str::FromStr};
-/// All the axepted tokens
+/// All the accepted tokens
 #[derive(Eq, PartialEq, Debug, Clone, Hash)]
 pub enum Token {
-    /// Any non other
+    /// Any other unknown chars not identified under
     Identifier(String),
+
+    /// A number with extra info
+    /// 
+    /// For all types of numbers
     NumberLiteral(NumberLiteral),
+
     /// String representation:`\`
-    /// for command starts and seperating matrixes
+    /// 
+    /// The Backslash: command starts and separating matrixes
     Backslash,
-    LeftCurlyBracket,  // {
-    RightCurlyBracket, // }
-    LeftBracket,       // [
-    RightBracket,      // ]
-    LeftParenthesis,   // (
-    RightParenthesis,  // )
-    Plus,              // +
-    Minus,             // -
-    Asterisk,          // *
-    Slash,             // /
-    Apostrophe,        // '
-    Underscore,        // _
-    Caret,             // ^
-    VerticalPipe,      // | and |
-    Comma,             // ,
-    Ampersand,         // &
-    Equals,            // =
-    EndOfContent,      // A special token that represents the end of content.
+
+    /// String representation:`{`
+    /// The left curly brackets: opening command expressions
+    LeftCurlyBracket,
+
+    /// String representation:`}`
+    /// 
+    /// The right curly bracket: closing command expressions
+    RightCurlyBracket,
+
+    /// String representation:`[`
+    /// 
+    /// The left bracket: degrees in sqrt
+    LeftBracket,
+
+    /// String representation:`]`
+    /// 
+    /// The right bracket: closing degrees in sqrt
+    RightBracket,
+
+    /// String representation:`(`
+    /// 
+    /// The right parenthesis: opening expressions
+    LeftParenthesis,
+
+    /// String representation:`)`
+    /// 
+    /// The right parenthesis: closing expressions 
+    RightParenthesis,
+
+    /// String representation:`+`
+    /// 
+    /// The plus sign: addition
+    Plus,
+    
+    /// String representation:`-`
+    /// 
+    /// The minus sign: subtraction 
+    Minus,
+    
+    /// String representation:`*`
+    /// 
+    /// The asterisk: multiplication 
+    Asterisk,
+    
+    /// String representation:`/`
+    /// 
+    /// The slash: division
+    Slash,
+    
+    /// String representation:`'`
+    /// 
+    /// The apostrophe: derivation
+    Apostrophe,
+    
+    /// String representation:`_`
+    /// 
+    /// The underscore: identification
+    Underscore,
+    
+    /// String representation:`^`
+    /// 
+    /// The caret: power of
+    Caret,
+    
+    /// String representation:`|`
+    /// 
+    /// The vertical pipe:absolute and determinant
+    VerticalPipe,
+
+    /// String representation:`,`
+    /// 
+    /// The comma: separating things in a vector
+    Comma,
+
+    /// String representation:`&`
+    /// 
+    /// The Ampersand: matrixes
+    Ampersand,
+
+    /// String representation:`=`
+    /// 
+    /// The equals: For equality
+    Equals,
+
+    /// No string representation
+    /// 
+    /// A special token that represents the end of content.
+    EndOfContent,
 }
 
 impl Display for Token {
@@ -62,19 +140,22 @@ impl Display for Token {
     }
 }
 impl Token {
+    ///if it is the ident variant and matching the string provided
     pub fn is_ident(&self, text: &str) -> bool {
         match self {
             Self::Identifier(val) => val == text,
             _ => false,
         }
     }
+    ///Takes a ref to the inner string of the ident
     pub fn take_ident(&self) -> Option<&String> {
         match self {
             Self::Identifier(v) => Some(v),
             _ => None,
         }
     }
-    pub fn is_eof(&self) -> bool {
+    ///is end of content
+    pub fn is_eoc(&self) -> bool {
         self == Token::EndOfContent
     }
 }
@@ -88,20 +169,17 @@ impl PartialEq<&Token> for Token {
         *self == **other
     }
 }
-
+///The number representation
 #[derive(Debug, Clone)]
 pub struct NumberLiteral {
+    ///the raw string without being parsed as a number
     pub raw: String,
+    ///The parsed value
     pub parsed: f64,
 }
 impl Display for NumberLiteral {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.parsed)
-    }
-}
-impl NumberLiteral {
-    pub fn reparse_from_raw(&mut self) {
-        self.parsed = self.raw.parse().expect("INTERNAL PARSING BUG")
     }
 }
 impl PartialEq for NumberLiteral {
