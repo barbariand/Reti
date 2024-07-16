78026a545eaa08ce8760ed5cc79b4d844ec16509
Alvinn8 <42838560+Alvinn8@users.noreply.github.com>
2024-07-16 10:48:48 +0200

    Bootstrap svelte project in reti-ui-common

---
+++ reti-ui-common/src/lib/Counter.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+let count: number = 0;
+
+const increment = () => {
+    count += 1;
+};
+
+console.log("hello");
+</script>
+
+<button on:click={increment}>
+    count is {count}
+</button>
+
+<style>
+button {
+    color: red;
+}
+</style>
