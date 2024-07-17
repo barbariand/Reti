db5aab40f16ac07dc5fe04553865fe4b33cd96ca
Alvinn8 <42838560+Alvinn8@users.noreply.github.com>
2024-07-17 11:24:17 +0200

    Add vitest

--- reti-ui-common/vite.config.ts
+++ reti-ui-common/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [svelte()],
+    test: {
+        environment: "happy-dom",
+        globals: true,
+        setupFiles: ["./src/vitest-setup.ts"],
+    },
 });
