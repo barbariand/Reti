4a24a3b0dbec04fd8ed893adb3d97ad04317b96c
Alvinn8 <42838560+Alvinn8@users.noreply.github.com>
2024-07-24 12:07:58 +0200

    Fix calculator-ui action paths

--- web-apps/calculator/src/App.test.ts
+++ web-apps/calculator/src/App.test.ts
@@ -3,8 +3,8 @@ import { render, screen } from "@testing-library/svelte";
 import App from "./App.svelte";
 
 describe("App", () => {
-    test("wip", () => {
+    test("Has (1) row", () => {
         render(App);
-        expect(screen.getByText(/work in progress/)).toBeInTheDocument();
+        expect(screen.getByText(/\(1\)/)).toBeInTheDocument();
     });
 });
